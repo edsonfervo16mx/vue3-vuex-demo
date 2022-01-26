acf7438cb2146996ade95ede67f5f794c91644db
edsonfervo16mx <edson55_@hotmail.com>
2022-01-25 23:39:03 -0500

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,41 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <!--<h1>Mi contador: {{ $store.state.contador }}</h1>-->
+    <h1 :style="colorContador">{{ titulo }} : {{ contador }}</h1>
+    <!--
+    <button @click="incrementar">Incrementar</button>
+    <button @click="accionIncrementar">Incrementar con accion</button>
+    -->
+    <BtnDisminuir />
+    <hr />
+    <BtnAccion :estado="true" :numero="20" />
+    <BtnAccion :estado="false" :numero="20" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { mapState, mapMutations, mapActions } from "vuex";
+import BtnDisminuir from "@/components/BtnDisminuir";
+import BtnAccion from "@/components/BotonAccion";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: { BtnDisminuir, BtnAccion },
+  data() {
+    return {
+      titulo: "Mi contador vuex",
+    };
+  },
+  computed: {
+    ...mapState(["contador"]),
+    colorContador() {
+      return [this.contador > 100 ? { color: "green" } : { color: "red" }];
+    },
+  },
+  methods: {
+    ...mapMutations(["incrementar"]),
+    ...mapActions(["accionIncrementar"]),
+  },
+};
 </script>
